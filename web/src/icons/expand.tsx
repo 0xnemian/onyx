422b696a58465c24ec9f9a53cd0ad0268ee536c6
Raunak Bhagat <r@rabh.io>
2025-11-14 02:43:53 -0800

    Edit icon

--- web/src/icons/expand.tsx
+++ web/src/icons/expand.tsx
@@ -12,6 +12,7 @@ const SvgExpand = (props: SVGProps<SVGSVGElement>) => (
   >
     <path
       d="M4.99994 5.49995L7.52858 2.97131C7.78891 2.71098 8.21105 2.71098 8.47138 2.97131L11 5.49995M5.00024 10.5L7.5288 13.0286C7.78914 13.2889 8.21127 13.2889 8.4716 13.0286L11.0002 10.5"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
