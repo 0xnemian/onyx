71ea29ad57a7ab36a07567de71917f453125151e
Raunak Bhagat <r@rabh.io>
2025-11-14 02:49:32 -0800

    Update collapsible-section component

--- web/src/refresh-components/CollapsibleSection.tsx
+++ web/src/refresh-components/CollapsibleSection.tsx
@@ -5,7 +5,7 @@ import Text from "@/refresh-components/texts/Text";
 import IconButton from "@/refresh-components/buttons/IconButton";
 import SvgFold from "@/icons/fold";
 import SvgExpand from "@/icons/expand";
-import { cn } from "@/lib/utils";
+import { cn, noProp } from "@/lib/utils";
 import { FieldLabel } from "./formik-fields/helpers";
 
 interface CollapsibleSectionProps {
@@ -24,9 +24,12 @@ export default function CollapsibleSection({
   const [isExpanded, setIsExpanded] = useState(defaultExpanded);
 
   return (
-    <div className="w-full flex flex-col gap-2">
+    <div className="w-full flex flex-col">
       {/* Header */}
-      <div className="flex flex-row items-center justify-between gap-4">
+      <div
+        className="flex flex-row items-center justify-between gap-4 cursor-pointer"
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
         {/* Left side: Title and Description */}
         <div className="flex flex-col gap-0.5">
           <Text mainUiAction text04>
@@ -43,7 +46,7 @@ export default function CollapsibleSection({
         <IconButton
           icon={isExpanded ? SvgFold : SvgExpand}
           internal
-          onClick={() => setIsExpanded(!isExpanded)}
+          onClick={noProp(() => setIsExpanded(!isExpanded))}
           tooltip={isExpanded ? "Collapse" : "Expand"}
         />
       </div>
@@ -53,8 +56,8 @@ export default function CollapsibleSection({
         className={cn(
           "overflow-hidden transition-all duration-300 ease-in-out",
           isExpanded
-            ? "z-10 opacity-100 translate-y-0"
-            : "-z-10 opacity-0 -translate-y-2"
+            ? "z-10 opacity-100 translate-y-0 pt-2"
+            : "-z-10 opacity-0 -translate-y-2 pt-0"
         )}
       >
         <div className="pt-1">{children}</div>
